a7361a9dee643eefff50a78350cd0604901a0982
njoyshadow <shs3740413@gmail.com>
2024-11-22 03:56:20 +0900

    MSession 추가

--- Extension/MSessionExtension.cs
+++ Extension/MSessionExtension.cs
@@ -1,5 +1,21 @@
-﻿namespace SteveLauncher.Extension;
+﻿using CmlLib.Core.Auth;
+using McLib.Auth.Model.Authentication;
+using McLib.Auth.Model.Minecraft;
+
+namespace SteveLauncher.Extension;
 
 public class MSessionExtension {
+    public MSessionExtension() {
+        
+    }
     
+    public static MSession Create(McUserProfile profile) {
+        return new MSession {
+            Username = profile.UserName,
+            UUID = profile.UUID,
+            AccessToken = profile.AccessToken,
+            UserType = "msa",
+            Xuid = profile.Xuid
+        };
+    }
 }
