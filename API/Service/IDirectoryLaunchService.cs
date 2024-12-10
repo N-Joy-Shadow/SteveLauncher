980513ef3a826960379cb0dc88f6dc50bef9f578
N-Joy-Shadow <57449485+N-Joy-Shadow@users.noreply.github.com>
2024-12-11 04:38:51 +0900

    refactoring 생각중...

--- API/Service/IDirectoryLaunchService.cs
+++ API/Service/IDirectoryLaunchService.cs
@@ -1,5 +1,9 @@
 ﻿namespace SteveLauncher.API.Service;
 
 public interface IDirectoryLaunchService {
+    /// <summary>
+    /// 외부 FileManager로 폴더를 여는 함수
+    /// </summary>
+    /// <param name="directoryPath">finder또는 explore로 열 폴더 경로</param>
     void Open(string directoryPath);
 }
